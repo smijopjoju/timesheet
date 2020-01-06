fe468a9a532f1f9beb754a95d0917e9a093b5d5f
smijopjoju <sj.2.3.4u@gmail.com>
2020-01-06 18:05:44 +0200

    Completed the model class with swagger description about the fields, avoiding setter for total time spend on the field, because It should be calculated based on the start and end time. We will do the calculation each time when we made change on the field

--- src/main/java/my/app/timesheet/domain/Entry.java
+++ src/main/java/my/app/timesheet/domain/Entry.java
@@ -14,28 +14,72 @@ public class Entry {
     @ApiModelProperty(name = "Task Identifier", dataType = "String", required = true, value = "The reference id to the task currently working on, eg: Jira ID")
     private String taskId;
 
-    @ApiModelProperty(name = "Task Description", dataType = "String", required = true, value = "")
+    @ApiModelProperty(name = "Task Description", dataType = "String", required = true, value = "Short description about the code task")
     private String taskDesc;
 
-    /**
-     * Field to record the date and time when we start doing the specified task
-     * */
+
+    @ApiModelProperty(name = "Start Date", dataType = "String", required = true, value = "Date and time when we start doing the specified task")
     private Date startDate;
 
-    /**
-     * Field to record the date and time when we completed the specified task
-     * */
+    @ApiModelProperty(name = "End Date", dataType = "String", required = true, value = "Date and time when we completed the specified task")
     private Date endDate;
 
-    /**
-     * Field to record the total time we spent on the specified task
-     * */
+    @ApiModelProperty(name = "Total time spend on the task", dataType = "String", required = true, value = "Capture the total time we spent on the specified task")
     private Date totalTimeTaken;
 
-    /**
-     * Field to store any comments regarding the task, like the status, pending task and
-     * if needed modified file names for future reference
-     * */
+    @ApiModelProperty(name = "Comments", dataType = "String", required = true, value = "Comments regarding the task, like the status, pending task and, " +
+            "if needed modified file names for future reference")
     private String comments;
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Date getTotalTimeTaken() {
+        return totalTimeTaken;
+    }
 }
